e09b44d5883fceff56bcc78f8178e3cc92ddc6c5
Markus-Dellacher <126161198+Markus-Dellacher@users.noreply.github.com>
2024-05-14 17:40:32 +0200

    Started with Login and Registration View

---
+++ src/main/java/edu/mci/fooddirector/views/LoginView.java
@@ -0,0 +1,50 @@
+package edu.mci.fooddirector.views;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@Route("login")
+@PageTitle("Login | Vaadin CRM")
+@AnonymousAllowed
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+
+    private final LoginForm login = new LoginForm();
+    private final Button registerButton = new Button("Register");
+
+    public LoginView(){
+        addClassName("login-view");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        login.setAction("login");
+
+        registerButton.addClickListener(e -> {
+            // Navigate to the registration view when the button is clicked
+            UI.getCurrent().navigate(RegistrationView.class);
+        });
+
+        add(new H1("Fooddirector"), login, new HorizontalLayout(registerButton));
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        // inform the user about an authentication error
+        if(beforeEnterEvent.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+            login.setError(true);
+        }
+    }
+}
+
