3b5fb96484b7792914d720e4f0dd79b6950edcdd
DanielBrandacher <d.brandacher@gmail.com>
2024-05-14 18:18:59 +0200

    Added Header to ReportView

--- src/main/java/edu/mci/fooddirector/views/report/ReportView.java
+++ src/main/java/edu/mci/fooddirector/views/report/ReportView.java
@@ -3,11 +3,14 @@ package edu.mci.fooddirector.views.report;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.selection.SingleSelect;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import edu.mci.fooddirector.model.domain.User;
 import edu.mci.fooddirector.model.domain.Address;
 import edu.mci.fooddirector.model.domain.Order;
@@ -20,8 +23,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 
-@PageTitle("Bericht")
+@PageTitle("Bericht | Fooddirector")
 @Route(value = "report", layout = MainLayout.class)
 
-public class ReportView {
+public class ReportView extends VerticalLayout {
+    public ReportView() {
+        //addClassNames("report");
+
+        H2 header = new H2("Bericht");
+        header.addClassNames(LumoUtility.Margin.Bottom.NONE, LumoUtility.Margin.Top.NONE, LumoUtility.FontSize.XXXLARGE, LumoUtility.Margin.Bottom.XLARGE);
+
+        //var content = new Main();
+        //content.addClassNames("cart-grid", LumoUtility.Gap.XLARGE, LumoUtility.AlignItems.START, LumoUtility.JustifyContent.EVENLY, LumoUtility.MaxWidth.FULL);
+        //content.addClassNames(Display.GRID, Gap.XLARGE, AlignItems.START, JustifyContent.EVENLY, MaxWidth.FULL);
+
+
+        add(header);
+        //add(content);
+    }
 }
