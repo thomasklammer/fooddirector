5ddc9ae5018e9daae0b2b74053d8c48fd3b25dcf
Thomas Klammer <thomasklammer.1993@gmail.com>
2024-05-14 16:36:29 +0200

    fixed

--- src/main/java/edu/mci/fooddirector/views/helloworld/HelloWorldView.java
+++ src/main/java/edu/mci/fooddirector/views/helloworld/HelloWorldView.java
@@ -1,35 +1,23 @@
 package edu.mci.fooddirector.views.helloworld;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.page.WebStorage;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
-import edu.mci.fooddirector.model.domain.Address;
-import edu.mci.fooddirector.model.domain.Account;
-import edu.mci.fooddirector.model.domain.CartItem;
-import edu.mci.fooddirector.model.enums.ArticleCategory;
-import edu.mci.fooddirector.model.services.CartService;
-import edu.mci.fooddirector.model.domain.User;
 import edu.mci.fooddirector.model.services.UserService;
 import edu.mci.fooddirector.views.MainLayout;
-
-import java.util.ArrayList;
 
 @PageTitle("Hello World")
 @Route(value = "hello-world", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
-
 public class HelloWorldView extends HorizontalLayout {
 
     private TextField name;
     private Button sayHello;
 
-    public HelloWorldView(CartService cartService) {
+    public HelloWorldView(UserService userService) {
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
