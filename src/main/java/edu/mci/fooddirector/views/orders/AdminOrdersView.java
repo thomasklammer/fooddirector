d38d223bee83bac193c6ea3889d719417f7b529d
Romed Kelmer <r.kelmer@gannomat.at>
2024-05-15 08:14:40 +0200

    Update AdminOrders and fix Dropdown

--- src/main/java/edu/mci/fooddirector/views/orders/AdminOrdersView.java
+++ src/main/java/edu/mci/fooddirector/views/orders/AdminOrdersView.java
@@ -4,8 +4,10 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import edu.mci.fooddirector.model.domain.Order;
@@ -16,16 +18,16 @@ import edu.mci.fooddirector.views.MainLayout;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @PageTitle("Admin Bestellungen")
 @Route(value = "Adminorders", layout = MainLayout.class)
-
 public class AdminOrdersView extends Div {
-    public static int i;
 
-    public AdminOrdersView(OrderService orderService){
+    private static final Logger LOGGER = Logger.getLogger(AdminOrdersView.class.getName());
 
+    public AdminOrdersView(OrderService orderService) {
         VerticalLayout layout = new VerticalLayout();
         layout.setClassName("custom-span");
         layout.setSpacing(false);
@@ -47,29 +49,45 @@ public class AdminOrdersView extends Div {
             return articleNames.toString();
         }).setHeader("Artikel").setSortable(true);
 
-        grid.addColumn(order -> order.getOrderStatus().toString())
-                .setHeader("Status")
-                .setSortable(true)
-                .setEditorComponent(order -> {
-                    ComboBox<OrderStatus> comboBox = new ComboBox<>();
-                    comboBox.setItems(OrderStatus.values());
-                    comboBox.setValue(order.getOrderStatus());
-                    comboBox.addValueChangeListener(event -> {
-                        order.setOrderStatus(event.getValue());
-                        orderService.saveOrder(order);
-                        grid.getDataProvider().refreshAll();
-                    });
-                    return comboBox;
-                });
+        Grid.Column<Order> statusColumn = grid.addColumn(order -> order.getOrderStatus().toString()).setHeader("Status").setSortable(true);
 
-        grid.setSelectionMode(Grid.SelectionMode.SINGLE);
+        Binder<Order> binder = new Binder<>(Order.class);
+        grid.getEditor().setBinder(binder);
+        grid.getEditor().setBuffered(true);
+
+        ComboBox<OrderStatus> comboBox = new ComboBox<>();
+        comboBox.setItems(OrderStatus.values());
+        binder.forField(comboBox).bind(Order::getOrderStatus, Order::setOrderStatus);
+        statusColumn.setEditorComponent(comboBox);
+
+        grid.addItemClickListener(event -> {
+            grid.getEditor().editItem(event.getItem());
+            comboBox.focus();
+            Notification.show("change value");
+        });
+
+        grid.getEditor().addCloseListener(event -> {
+            if (binder.hasChanges()) {
+                Order order = event.getItem();
+                try {
+                    if (order.getOrderStatus() == null) {
+                        Notification.show("Status cannot be null");
+                    } else {
+                        Notification.show("try to save");
+                        orderService.saveOrder(order);
+                        grid.getDataProvider().refreshItem(order);
+                        Notification.show("Order successfully saved");
+                    }
+                } catch (Exception e) {
+                    Notification.show("Internal error. Please notify the administrator.");
+                }
+            }
+        });
 
         layout.add(grid);
-
         layout.setWidth("auto");
         layout.setMargin(false);
         layout.setAlignItems(FlexComponent.Alignment.STRETCH);
-
 
         Div container = new Div();
         container.add(layout);
@@ -79,5 +97,4 @@ public class AdminOrdersView extends Div {
 
         add(container);
     }
-
 }
